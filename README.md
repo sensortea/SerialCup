96d14928c228e01006cbe72d576e2d842dd03c27
alexteaa <alexteaa@users.noreply.github.com>
2024-01-25 11:59:31 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
 # SerialCup
-Handy tool to save and query/format serial port data (e.g. into CSV)
+
+SerialCup is a handy small script to save and query/format serial port data (from Arduino/ESP32/etc). E.g. when extracting (especially repeatedly) CSV file of a sensor readings for multiple tests. Avoids scrolling in and copy-pasting from Serial Monitor, but not just that. You can optionally control formatting by supplying regular expressions or by writing some Python code (for more custom control, if really need to). Runs on Windows, MacOS, Linux, etc.
+
+Has two main functions:
+* capture: captures data from serial port into a file
+    * prefixes lines with a timestamp
+    * rolls to a new file every 1h to keep size manageable (keeps all history)
+    * allows to pause/resume capturing with one key press
+    * allows to insert a line into captured data (e.g. to "label" boundaries of different tests)
+* query: extracts specified range from captured data files and optionally formats the result
+    * range can be time range or "between labels"
+    * can filter lines using standard regex
+    * can format output using standard match+replace regex
+
+As an additional perk, since data is saved locally and this is a Python "lib", you can query and process data with Python script for further flexibility and control. It's also easy to do stats and even ML with standard Python tools like Pandas, NumPy, and in Jupyter Notebooks.
+
+### Quick how-to
+
+Python and `pyserial` lib are required. Once Python is installed, you can install `pyserial` with
+
+```commandline
+pip3 install pyserial
+```
+
+Connect a thing (e.g. Arduino/ESP32/etc.) via USB. List ports with:
+
+```commandline
+% python3 serialcup.py list
+
+/dev/cu.Bluetooth-Incoming-Port - n/a S/N: None
+/dev/cu.usbmodem101 - IOUSBHostDevice S/N: 242353135363516111A1
+```
+
+Pick destination for saving the data, your device's S/N and baudrate, and start capturing:
+
+```commandline
+% python3 serialcup.py capture /tmp/serialcup 242353135363516111A1 9600
+
+> STARTED capture
+> - Hit <Enter> to pause/resume
+> - Type text and hit <Enter> to insert a line into a file with captured data
+test1-start
+> INSERTED: 1705318467232,test1-start
+test1-end
+> INSERTED: 1705318472233,test1-end
+^C
+> STOPPED capture
+```
+
+In the above the `test1-start` and `test1-stop` lines where inserted into the data (by entering the lines while data was captured) to "label" the start and end of the test.
+
+The data printed to serial port is saved into files prefixed with epochMs:
+
+```text
+...
+1705318466230,Measurement #1
+1705318466230,temp:13.90,humi:32.50
+1705318467232,test1-start
+1705318468234,Measurement #2
+1705318469234,temp:14.00,humi:33.00
+1705318470235,Measurement #3
+1705318471235,temp:14.10,humi:33.50
+1705318472233,test1-end
+1705318473235,Measurement #3
+...
+```
+
+To extract lines between `test1-start` and `test1-end`:
+```commandline
+% python3 serialcup.py query /tmp/serialcup 242353135363516111A1 test1-start test1-end
+
+1705318468234,Measurement #2
+1705318469234,temp:14.00,humi:33.00
+1705318470235,Measurement #3
+1705318471235,temp:14.10,humi:33.50
+```
+
+You can also specify any of the start or the end of the range using the time instead of "label". The script accepts any of: unix timestamp (seconds or milliseconds since 1970-01-01 00:00:00 UTC); datetime in `YYYY-MM-DD HH:MI:SS` format; `now` and `now-10m` notation (`d` - days, `h` - hours, `m` - minutes, `s` - seconds).
+
+To filter only the lines with `temp` you add a filter regex argument:
+
+```commandline
+% python3 serialcup.py query /tmp/serialcup 242353135363516111A1 test1-start test1-end '.*temp.*'
+
+1705318469234,temp:14.00,humi:33.00
+1705318471235,temp:14.10,humi:33.50
+```
+
+Now let's format the lines as rows in CSV file:
+
+```commandline
+% python3 serialcup.py query /tmp/serialcup 242353135363516111A1 test1-start test1-end '([0-9]+),.*temp:([0-9]+[.]*[0-9]*).*' '\1,\2'
+
+1705318469234,14.00
+1705318471235,14.10
+```
+
+In this case, `([0-9]+),.*temp:([0-9]+[.]*[0-9]*).*` matches the lines with `temp:` and additionally captures the timestamp (`([\d]+)`) and a temp reading value (`([\d.]+)`) as groups, while `\1,\2` instructs to output the captured groups with comma between them.
+
+Now, to tie all of this together, to extract a CSV file with temperature readings between `test1-start` and `test1-end` marks, that looks like this:
+```text
+epochMs,temp
+1705318469234,14.00
+1705318471235,14.10
+```
+do:
+```commandline
+echo "epochMs,temp" > /tmp/temp.csv &&
+python3 serialcup.py query /tmp/serialcup 242353135363516111A1 test1-start test1-end '([0-9]+),.*temp:([0-9]+[.]*[0-9]*).*' '\1,\2' >> /tmp/temp.csv
+```
+
+If you'd rather write Python code to process the data, you can do that as well. E.g. this code replaces epochMs with ms passed since previous recording and prints out the lines:
+
+```python
+import serialcup
+
+def processFunc(line):
+  global prev_epoch_ms
+  epoch_ms = int(line[:13])
+  delta_ms = epoch_ms - prev_epoch_ms
+  prev_epoch_ms = epoch_ms
+  print(str(delta_ms) + line[13:])
+
+serialcup.query('/tmp/serialcup', '242353135363516111A1', 'now-10m', 'now', processFunc)
+```
+
+On the way out, if you found this useful, you may also want to check out https://github.com/sensortea/CupLogger - a more sophisticated tool to capture and visualize serial port data.
+
+### <a name="support"></a>Follow and support
+
+Follow this project or [sensortea](https://github.com/sensortea) on github or [@sensortea](https://twitter.com/sensortea) on Twitter (X).
+
+A great help is sharing what didn't work for you or what must be improved.
+
+The best help is sharing if this project is useful, e.g. by starring it here on Github. You could also send a note to info_at_sensortea_dot_com.
+
+### <a name="license"></a> License
+
+This project is distributed under the MIT License. See the LICENSE file for more details.
---
+++ serialcup.py
@@ -0,0 +1,235 @@
+import datetime
+import os
+import queue
+import re
+import serial
+import sys
+import threading
+import time
+from serial.tools import list_ports
+
+MAX_FILE_SEC = 3600
+
+def list_serial_ports():
+    available_ports = [port for port in list_ports.comports()]
+    for port in available_ports:
+        print(f"{port} S/N: {port.serial_number}")
+
+def get_port_by_sn(serial_number):
+    for port in list_ports.comports():
+        if port.serial_number == serial_number:
+            return port
+    return None
+
+def epoch_ms():
+    return int(round(time.time() * 1000))
+
+def to_epoch_ms(arg):
+    # Epoch in seconds (10 digits)
+    if arg.isdigit() and len(arg) == 10:
+        return int(arg) * 1000
+
+    # Epoch in milliseconds (13 digits)
+    elif arg.isdigit() and len(arg) == 13:
+        return int(arg)
+
+    # Formatted date time
+    elif re.match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", arg):
+        dt = datetime.datetime.strptime(arg, "%Y-%m-%d %H:%M:%S")
+        return int(dt.timestamp() * 1000)
+
+    # Current time
+    elif arg == "now":
+        return int(datetime.datetime.now().timestamp() * 1000)
+
+    # Time subtraction
+    elif re.match(r"now-\d+[smhd]", arg):
+        now = datetime.datetime.now()
+        amount, unit = int(re.search(r"\d+", arg).group()), arg[-1]
+        if unit == 's':
+            delta = datetime.timedelta(seconds=amount)
+        elif unit == 'm':
+            delta = datetime.timedelta(minutes=amount)
+        elif unit == 'h':
+            delta = datetime.timedelta(hours=amount)
+        elif unit == 'd':
+            delta = datetime.timedelta(days=amount)
+        else:
+            raise ValueError("Invalid time unit in argument.")
+        return int((now - delta).timestamp() * 1000)
+
+    else:
+        return None
+
+def capture_serial_data(base_dir, serial_number, baud_rate):
+    os.makedirs(base_dir, exist_ok=True)
+    port = get_port_by_sn(serial_number)
+    if not port:
+        print("ERROR: No port found with device S/N " + serial_number)
+        exit(1)
+    ser = serial.Serial(port.device, baud_rate, timeout=1)
+
+    user_input_queue = queue.Queue()
+    is_paused = False
+
+    def user_input_thread():
+        while True:
+            user_input = input()  # Wait for user input
+            user_input_queue.put(user_input)
+
+    threading.Thread(target=user_input_thread, daemon=True).start()
+
+    def new_file_path():
+        nonlocal base_dir, serial_number
+        return os.path.join(base_dir, f"{serial_number}.{time.strftime('%Y_%m_%d_%H%M')}.{round(time.time())}.txt")
+
+    file_start_time = time.time()
+    current_file = open(new_file_path(), 'a')
+
+    def write(data):
+        nonlocal current_file, file_start_time
+        current_time = time.time()
+        if current_time - file_start_time >= MAX_FILE_SEC:
+            current_file.close()
+            current_file = open(new_file_path(), 'a')
+            file_start_time = current_time
+
+        line = str(epoch_ms()) + ',' + data + '\n'
+        current_file.write(line)
+        current_file.flush()
+        return line
+
+    try:
+        print('> STARTED capture')
+        print('> - Hit <Enter> to pause/resume')
+        print('> - Type text and hit <Enter> to insert a line into a file with captured data')
+        while True:
+            # avoiding busy loop when paused: it can only be unpaused with input
+            while not user_input_queue.empty() or is_paused:
+                user_input = user_input_queue.get()
+                # Toggle pause on empty line (user presses "enter")
+                if user_input == "":
+                    is_paused = not is_paused
+                    if is_paused:
+                        print("> PAUSED capture")
+                    else:
+                        print("> RESUMED capture")
+                else:
+                    line = write(user_input)
+                    print("> INSERTED: " + line[:-1])
+
+            if not is_paused:
+                # Capture and write serial data to the file
+                data = ser.readline().decode().strip()
+                if data:
+                    write(data)
+
+    except KeyboardInterrupt:
+        print()
+        print("> STOPPED capture")
+    finally:
+        ser.close()
+
+
+def query(dir, serial_number, range_start, range_end, processFunc):
+    start_epoch_ms = to_epoch_ms(range_start)
+    start_mark = range_start if not start_epoch_ms else None
+    inside_mark = False if start_mark else True
+    end_epoch_ms = to_epoch_ms(range_end)
+    end_mark = range_end if not end_epoch_ms else None
+    for filename in sorted(os.listdir(dir)):
+        if filename.startswith(serial_number):
+            file_epoch_sec = int(filename.split('.')[2])
+            if start_epoch_ms and (file_epoch_sec + MAX_FILE_SEC) < start_epoch_ms // 1000:
+                continue
+            if end_epoch_ms and file_epoch_sec > end_epoch_ms // 1000:
+                continue
+
+            with open(os.path.join(dir, filename), 'r') as file:
+                for line in file:
+                    line_parts = line.strip().split(',', 1)
+                    line_epoch_ms = int(line_parts[0])
+
+                    if start_epoch_ms and line_epoch_ms < start_epoch_ms:
+                        continue
+                    if end_epoch_ms and line_epoch_ms > end_epoch_ms:
+                        continue
+
+                    if not inside_mark:
+                        if start_mark == line_parts[1]:
+                            inside_mark = True
+                            continue
+                    else:
+                        if end_mark == line_parts[1]:
+                            inside_mark = False
+                            continue
+
+                    if not inside_mark:
+                        continue
+
+                    processFunc(line.strip())
+                    
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: serialcup.py <command> [options]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+
+    if command == 'list':
+        list_serial_ports()
+    elif command == 'capture':
+        if len(sys.argv) != 5:
+            print("""
+Usage: serialcup.py capture <dir> <serial_number> <baud_rate>""")
+            sys.exit(1)
+        base_dir = sys.argv[2]
+        serial_number = sys.argv[3]
+        baud_rate = sys.argv[4]
+        capture_serial_data(base_dir, serial_number, baud_rate)
+    elif command == 'query':
+        if len(sys.argv) < 6 or len(sys.argv) > 8:
+            print("""
+Usage: serialcup.py query <dir> <serial_number> <range_start> <range_end> [<match_regex> [<replace_str>]]
+
+Arguments:
+  <dir>            - Directory with captured data.
+  <serial_number>  - Serial number of the device.
+  <range_start>    - Start of the selection range, specified as:
+                     - Label string
+                     - Time, in various formats:
+                       * Unix timestamp (seconds or milliseconds since 1970-01-01 00:00:00 UTC)
+                       * Datetime in 'YYYY-MM-DD HH:MI:SS' format
+                       * 'now' and 'now-10m' notation (where 'd' = days, 'h' = hours, 'm' = minutes, 's' = seconds)
+  <range_end>      - End of the selection range, specified in the same formats as <range_start>.
+
+Optional Arguments:
+  [<match_regex>]  - A regex pattern to filter the lines.
+  [<replace_str>]  - A string for formatting the line. Supports groups captured in <match_regex>.
+
+Examples:
+  serialcup.py query /tmp/serialcup 242353135363516111A1 now-1h now
+  serialcup.py query /tmp/serialcup 242353135363516111A1 label1 label2 '([0-9]+),.*' 'timestamp:\1'""")
+            sys.exit(1)
+        base_dir = sys.argv[2]
+        serial_number = sys.argv[3]
+        range_start = sys.argv[4]
+        range_end = sys.argv[5]
+        match_regex = re.compile(sys.argv[6]) if len(sys.argv) > 6 else None
+        replace_str = sys.argv[7] if len(sys.argv) > 7 else None
+
+        def printFunc(line):
+            nonlocal match_regex, replace_str
+            if match_regex and not match_regex.search(line):
+                return
+            if match_regex and replace_str:
+                print(match_regex.sub(replace_str, line))
+                return
+            print(line)
+
+        query(base_dir, serial_number, range_start, range_end, printFunc)
+    else:
+        print("Invalid command. Use 'list' or 'capture'.")
+
+if __name__ == '__main__':
+    main()
